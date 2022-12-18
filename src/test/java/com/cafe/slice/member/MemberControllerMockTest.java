3d0200cb0ab8673980a59e8f7ab2937ecbf9d1d8
Ksiyeong <yuiop2767@naver.com>
2022-12-18 12:47:42 +0900

    Test: 슬라이스 테스트 케이스 작성
    MemberControllerMockTest - postMemberTest 완료

---
+++ src/test/java/com/cafe/slice/member/MemberControllerMockTest.java
@@ -0,0 +1,79 @@
+package com.cafe.slice.member;
+
+import com.cafe.member.*;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MemberControllerMockTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Gson gson;
+
+    @MockBean
+    private MemberService memberService;
+
+    @MockBean
+    private MemberMapper mapper;
+
+    @Test
+    void postMemberTest() throws Exception {
+        //given
+        MemberDto.Post post = new MemberDto.Post(
+                "test@test.com",
+                "테스트",
+                "010-1234-5678"
+        );
+
+        MemberDto.Response response = new MemberDto.Response(
+                1L,
+                post.getEmail(),
+                post.getName(),
+                post.getPhone()
+        );
+
+        given(mapper.memberPostDtoToMember(Mockito.any(MemberDto.Post.class))).willReturn(new Member());
+        given(memberService.createMember(Mockito.any(Member.class))).willReturn(new Member());
+        given(mapper.memberToMemberResponseDto(Mockito.any(Member.class))).willReturn(response);
+
+        String content = gson.toJson(post);
+
+        //when
+        ResultActions actions =
+                mockMvc.perform(
+                        post("/members")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(content)
+                );
+
+        //then
+        MvcResult result = actions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.data.email").value(post.getEmail()))
+                .andExpect(jsonPath("$.data.name").value(post.getName()))
+                .andExpect(jsonPath("$.data.phone").value(post.getPhone()))
+                .andReturn();
+
+        System.out.println("\nresult = " + result.getResponse().getContentAsString() + "\n");
+
+    }
+}
