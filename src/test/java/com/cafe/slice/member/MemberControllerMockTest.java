7b14d09bbae5f434788821ef78b4e6e5cfafda75
Ksiyeong <yuiop2767@naver.com>
2022-12-18 22:37:13 +0900

    Docs: api 문서화
    MemberController api 문서화

--- src/test/java/com/cafe/slice/member/MemberControllerMockTest.java
+++ src/test/java/com/cafe/slice/member/MemberControllerMockTest.java
@@ -5,14 +5,16 @@ import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -21,19 +23,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willReturn;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(MemberController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureRestDocs
 public class MemberControllerMockTest {
     @Autowired
     private MockMvc mockMvc;
@@ -83,6 +84,27 @@ public class MemberControllerMockTest {
                 .andExpect(jsonPath("$.data.email").value(post.getEmail()))
                 .andExpect(jsonPath("$.data.name").value(post.getName()))
                 .andExpect(jsonPath("$.data.phone").value(post.getPhone()))
+                .andDo(document(
+                        "post-member",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("name").type(JsonFieldType.STRING).description("이름"),
+                                        fieldWithPath("phone").type(JsonFieldType.STRING).description("휴대폰 번호")
+                                )
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                        fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("data.name").type(JsonFieldType.STRING).description("이름"),
+                                        fieldWithPath("data.phone").type(JsonFieldType.STRING).description("휴대폰 번호")
+                                )
+                        )
+                ))
                 .andReturn();
 
         System.out.println("\nresult = " + result.getResponse().getContentAsString() + "\n");
@@ -110,7 +132,7 @@ public class MemberControllerMockTest {
 
         //when
         ResultActions actions = mockMvc.perform(
-                patch("/members/" + response.getMemberId())
+                patch("/members/{member-id}", response.getMemberId())
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(content)
@@ -120,6 +142,28 @@ public class MemberControllerMockTest {
         MvcResult result = actions
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.name").value(patch.getName()))
+                .andDo(document(
+                        "patch-member",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(parameterWithName("member-id").description("회원 식별자")),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("email").type(JsonFieldType.STRING).description("이메일").optional(),
+                                        fieldWithPath("name").type(JsonFieldType.STRING).description("이름").optional(),
+                                        fieldWithPath("phone").type(JsonFieldType.STRING).description("휴대폰 번호").optional()
+                                )
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                        fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("data.name").type(JsonFieldType.STRING).description("이름"),
+                                        fieldWithPath("data.phone").type(JsonFieldType.STRING).description("휴대폰 번호")
+                                )
+                        )
+                ))
                 .andReturn();
 
         System.out.println("\nresult = " + result.getResponse().getContentAsString() + "\n");
@@ -141,7 +185,7 @@ public class MemberControllerMockTest {
 
         //when
         ResultActions actions = mockMvc.perform(
-                get("/members/" + memberId)
+                get("/members/{member-id}", memberId)
                         .accept(MediaType.APPLICATION_JSON)
         );
 
@@ -152,6 +196,20 @@ public class MemberControllerMockTest {
                 .andExpect(jsonPath("$.data.email").value(email))
                 .andExpect(jsonPath("$.data.name").value(name))
                 .andExpect(jsonPath("$.data.phone").value(phone))
+                .andDo(document(
+                        "get-member",
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(parameterWithName("member-id").description("회원 식별자")),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                        fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("data.email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("data.name").type(JsonFieldType.STRING).description("이름"),
+                                        fieldWithPath("data.phone").type(JsonFieldType.STRING).description("휴대폰 번호")
+                                )
+                        )
+                ))
                 .andReturn();
 
         System.out.println("\nresult = " + result.getResponse().getContentAsString() + "\n");
@@ -163,7 +221,7 @@ public class MemberControllerMockTest {
         //given
         int page = 1;
         int size = 5;
-        long totalElements = 10;
+        long totalElements = 9;
 
         // memberService 의 리턴값 생성
         List<Member> members = new ArrayList<>();
@@ -176,8 +234,13 @@ public class MemberControllerMockTest {
 
         // mapper 의 리턴값 생성
         List<MemberDto.Response> responses = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            responses.add(new MemberDto.Response());
+        for (long i = totalElements; i >= size; i--) {
+            responses.add(new MemberDto.Response(
+                    i,
+                    "test" + i + "@test.com",
+                    "테스트" + i,
+                    "010-0000-000" + i
+            ));
         }
 
         given(memberService.findMembers(Mockito.anyInt(), Mockito.anyInt())).willReturn(pageMembers);
@@ -185,7 +248,7 @@ public class MemberControllerMockTest {
 
         //when
         ResultActions actions = mockMvc.perform(
-                get("/members?page=" + page + "&size=" + size)
+                get("/members?page={page}&size={size}", page, size)
                         .accept(MediaType.APPLICATION_JSON)
         );
 
@@ -195,6 +258,28 @@ public class MemberControllerMockTest {
                 .andExpect(jsonPath("$.data").isArray())
                 .andExpect(jsonPath("$.pageInfo.page").value(page))
                 .andExpect(jsonPath("$.pageInfo.size").value(size))
+                .andDo(document(
+                        "get-members",
+                        preprocessResponse(prettyPrint()),
+                        queryParameters(
+                                parameterWithName("page").description("출력할 페이지"),
+                                parameterWithName("size").description("페이지당 게시물 수")
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.ARRAY).description("결과 데이터"),
+                                        fieldWithPath("data[*].memberId").type(JsonFieldType.NUMBER).description("회원 식별자"),
+                                        fieldWithPath("data[*].email").type(JsonFieldType.STRING).description("이메일"),
+                                        fieldWithPath("data[*].name").type(JsonFieldType.STRING).description("이름"),
+                                        fieldWithPath("data[*].phone").type(JsonFieldType.STRING).description("휴대폰 번호"),
+                                        fieldWithPath("pageInfo").type(JsonFieldType.OBJECT).description("페이지 정보"),
+                                        fieldWithPath("pageInfo.page").type(JsonFieldType.NUMBER).description("현재 페이지"),
+                                        fieldWithPath("pageInfo.size").type(JsonFieldType.NUMBER).description("한 페이지에 노출할 데이터 갯수"),
+                                        fieldWithPath("pageInfo.totalElements").type(JsonFieldType.NUMBER).description("총 데이터 갯수"),
+                                        fieldWithPath("pageInfo.totalPages").type(JsonFieldType.NUMBER).description("총 페이지 갯수")
+                                )
+                        )
+                ))
                 .andReturn();
 
         System.out.println("\nresult = " + result.getResponse().getContentAsString() + "\n");
@@ -209,11 +294,18 @@ public class MemberControllerMockTest {
 
         //when
         ResultActions actions = mockMvc.perform(
-                delete("/members/" + memberId)
+                delete("/members/{member-id}", memberId)
         );
 
         //then
-        MvcResult result = actions.andExpect(status().isNoContent()).andReturn();
+        MvcResult result = actions
+                .andExpect(status().isNoContent())
+                .andDo(document(
+                        "delete-member",
+//                        preprocessResponse(prettyPrint()),
+                        pathParameters(parameterWithName("member-id").description("회원 식별자"))
+                ))
+                .andReturn();
 
         System.out.println("\nresult = " + result.getResponse().getContentAsString() + "\n");
     }
